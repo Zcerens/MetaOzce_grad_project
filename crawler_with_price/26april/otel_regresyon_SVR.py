648150ce92155576ac86c3d3f358b540362be8c2
Zcerens <matricariaxx@gmail.com>
2024-04-29 22:11:28 +0300

    Fair-price kısmı için fiyatların sonundaki gereksiz fazla basamaklar kaldırıldı, yuvarlama işlemi ile

--- crawler_with_price/26april/otel_regresyon_SVR.py
+++ crawler_with_price/26april/otel_regresyon_SVR.py
@@ -11,27 +11,27 @@ from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
-# CSV dosyasından verileri okuyun
+
 df = pd.read_csv("duzenlenmis_otel_veritabani_DB3.csv")
 
-# Eksik değerlere sahip gözlemleri veri setinden çıkarın
+
 df.dropna(inplace=True)
 
-# Eğitim ve test setlerini oluşturun
+# Eğitim ve test setleri
 X = df.drop(['otel_ad', 'fiyat', 'il', 'ilce'], axis=1) # Bağımsız değişkenler
 y = df['fiyat'] # Bağımlı değişken
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# SVR modelini eğitin
+# SVR modelini
 model = SVR(kernel='linear')  # Linear kernel kullanarak SVR modeli
 model.fit(X_train, y_train)
 
-# Eğitim ve test setleri üzerinde tahmin yapın
+# Eğitim ve test setleri üzerinde tahmin
 y_train_pred = model.predict(X_train)
 y_test_pred = model.predict(X_test)
 
-# Hata metriklerini hesaplayın
+# Hata metriklerini hesap
 train_rmse = mean_squared_error(y_train, y_train_pred, squared=False)
 test_rmse = mean_squared_error(y_test, y_test_pred, squared=False)
 
@@ -44,19 +44,19 @@ for i, feature in enumerate(X.columns):
     print(feature + ":", model.coef_[0][i])
     
 
-# Destek vektörlerinin katsayılarını alın
+# Destek vektörlerinin katsayıları
 coefficients = model.coef_[0]
 
 # Özellik isimlerini alın
 features = X.columns
 
-# Özelliklerin destek vektörlerine katkısını gösteren bir çubuk grafiği oluşturun
-plt.figure(figsize=(24, 10))
-plt.bar(features, coefficients, color='blue')
+# Özelliklerin destek vektörlerine katkısını gösteren bir plot
+plt.figure(figsize=(16, 8))
+plt.bar(features, coefficients, color='green')
 plt.xlabel('Özellikler')
 plt.ylabel('Destek Vektör Katsayıları')
 plt.title('Özelliklerin Destek Vektörlerine Katkısı')
-plt.xticks(rotation=90)
+plt.xticks(rotation=80)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
 
@@ -65,11 +65,17 @@ plt.show()
 # Step 1: Make Your Predictions
 predicted_prices = model.predict(X)
 
+# Round the predicted prices to one decimal place
+predicted_prices_rounded = [round(abs(price), 1) * (-1 if price < 0 else 1) for price in predicted_prices]
+
 # Step 2: Compare Predictions with Actual Prices and Add Hotel Names
-price_comparison = pd.DataFrame({'Hotel Name': df['otel_ad'], 'Actual Price': y, 'Predicted Price': predicted_prices})
+price_comparison = pd.DataFrame({'Hotel Name': df['otel_ad'], 'Actual Price': y, 'Predicted Price': predicted_prices_rounded})
 
 # Step 3: Analyze the Calculated Price Difference
 price_comparison['Price Difference'] = price_comparison['Actual Price'] - price_comparison['Predicted Price']
+
+# Round the price differences to one decimal place
+price_comparison['Price Difference'] = [round(abs(diff), 1) * (-1 if diff < 0 else 1) for diff in price_comparison['Price Difference']]
 
 # Step 4: Fine-Tuning
 fair_price_threshold = 500  # Let's set a threshold, for example, 500 TL
